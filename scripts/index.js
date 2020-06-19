8431b95b231c36d7fcdba70ef665c63156788389
Michail Kotelnikov <kotelnickov.mischa@yandex.ru>
2020-06-20 01:33:57 +0300

    second commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,52 +1,37 @@
 const popup = document.querySelector('.popup');
-const popupOpenButton = document.querySelector('.profile__edit');
+const editProfileButton = document.querySelector('.profile__edit');
 const popupCloseButton = popup.querySelector('.popup__close');
+const submitButton = document.querySelector('.popup__forms');
+const nameInput = document.querySelector('.popup__input_type_name');
+const jobInput = document.querySelector('.popup__input_type_job');
+
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__surname');
 
 
-const popupToggle = function () {
-    popup.classList.toggle('popup_opened');
+
+const openPopup = function() {
+    popup.classList.add('popup_opened');
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
+
 }
 
-popupOpenButton.addEventListener('click', popupToggle);
-popupCloseButton.addEventListener('click', popupToggle);
+
+const closePopup = function() {
+    popup.classList.remove('popup_opened');
+}
 
 
-// Находим форму в DOM
-let popupSubmitButton = document.querySelector('.popup__forms');  // Воспользуйтесь методом querySelector()
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
 function formSubmitHandler(evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-    // Так мы можем определить свою логику отправки.
-    // О том, как это делать, расскажем позже.
-
-    // Находим поля формы в DOM
-    let nameInput = document.querySelector('.popup__input_type_name');  // Воспользуйтесь инструментом .querySelector()
-    let jobInput = document.querySelector('.popup__input_type_job'); // Воспользуйтесь инструментом .querySelector()
-
-    // Получите значение полей из свойства value
-
-   
-
-    // Выберите элементы, куда должны быть вставлены значения полей
-    let profileName = document.querySelector('.profile__name');
-    let profileJob = document.querySelector('.profile__surname');
-
-
-    // Вставьте новые значения с помощью textContent
+    evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
 
-   popupToggle();
-
+    closePopup();
 }
 
-
-
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-popupSubmitButton.addEventListener('submit', formSubmitHandler);
-
-
+editProfileButton.addEventListener('click', openPopup);
+popupCloseButton.addEventListener('click', closePopup);
+submitButton.addEventListener('submit', formSubmitHandler);
 
